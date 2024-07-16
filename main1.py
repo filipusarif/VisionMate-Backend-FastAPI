9fde34b10ea91fab1f66623b4132523e141bf8a2
filipusarif <filipus123.gmil@gmail.com>
2024-07-16 19:43:42 +0700

    test

--- main1.py
+++ main1.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI, UploadFile, File
-from fastapi.responses import JSONResponse
-from fastapi.middleware.cors import CORSMiddleware
-from PIL import Image
+# from fastapi.responses import JSONResponse
+# from fastapi.middleware.cors import CORSMiddleware
+# from PIL import Image
 # import torch
-import cv2
-import numpy as np
-import io
+# import cv2
+# import numpy as np
+# import io
 
 app = FastAPI()
 
@@ -14,21 +14,21 @@ origins = [
     "http://localhost:5173",  # Tambahkan URL frontend React kamu di sini
 ]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 # Load YOLO model
 # model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
 
 @app.post("/detect/")
 async def detect(file: UploadFile = File(...)):
-    image = Image.open(io.BytesIO(await file.read()))
-    image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+    # image = Image.open(io.BytesIO(await file.read()))
+    # image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
     # results = model(image)
     # detections = results.pandas().xyxy[0].to_dict(orient="records")
     return "JSONResponse(content=detections)"
