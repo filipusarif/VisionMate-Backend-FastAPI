2296579bc7c19e67da70aaebcc9c23cd4c8e1a57
filipusarif <filipus123.gmil@gmail.com>
2024-07-16 19:53:57 +0700

    j

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 from PIL import Image
-import cv2
 import numpy as np
 import io
 
