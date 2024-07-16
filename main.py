3a518b0e723d61966f5a63dcc2298e39373e0618
filipusarif <filipus123.gmil@gmail.com>
2024-07-16 19:57:32 +0700

    j

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
-import numpy as np
 
 
 app = FastAPI()
